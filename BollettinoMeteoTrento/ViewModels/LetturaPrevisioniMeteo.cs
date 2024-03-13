cafe2deec2af310367867d33a927216a4f3da081
Davide-Lot <davide.lotito@studenti.unicam.it>
2024-03-13 22:44:15 +0100

    Aggiunta la cartella ViewModels con LetturaPrevisioniMeteo e index per la cartella Views

---
+++ BollettinoMeteoTrento/ViewModels/LetturaPrevisioniMeteo.cs
@@ -0,0 +1,17 @@
+﻿using BollettinoMeteoTrento.Modelli;
+
+namespace BollettinoMeteoTrento.ViewModels
+{
+    public class LetturaPrevisioniMeteo
+    {
+        // Proprietà pubblica chiamata 'modello' di tipo Root, rappresenta il modello o la struttura dati delle previsioni meteo
+        public Root modello { get; set; }
+
+        // Costruttore della classe che accetta un parametro di tipo Root chiamato 'modello'
+        public LetturaPrevisioniMeteo(Root modello)
+        {
+            // Inizializzazione della proprietà 'modello' con il valore passato come parametro nel costruttore
+            this.modello = modello;
+        }
+    }
+}
