8c3a38dd0e768b7f06c07c5b4a959e9c91850846
Federica Salvucci <federica.salvucci@studenti.unicam.it>
2024-03-14 17:34:57 +0100

    Implementato il Program.cs nel servizio SOAP

--- BollettinoMeteoTrento.SOAP/Program.cs
+++ BollettinoMeteoTrento.SOAP/Program.cs
@@ -1,6 +1,22 @@
+using SoapCore;
+using BollettinoMeteoTrento.SOAP.BusinessLogic;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder. Services. AddSoapCore();
+builder.Services.AddScoped<ISoapService, SoapService>0);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.UseSoapEndpoint<ISOAPService>("/Service.wsdl", new SoapEncoderOptions(), SoapSerializer.XmlSerializer);
+});
+
+app.Run();
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
